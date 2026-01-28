8763d140cfb665d8ada362de54618df4a1d66765
aAmareus <gonzalezamareus@gmail.com>
2026-01-28 00:31:20 -0300

    Starting portfolio

---
+++ app/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="hero-section">
+        <h1>Amaro González</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
